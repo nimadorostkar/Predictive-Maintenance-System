1210d664131a69512111335e90fa35df89961080
nima <54316106+nimadorostkar@users.noreply.github.com>
2024-10-11 15:38:32 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
+
 # Load the trained model (replace 'model.pkl' with your actual file name)
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
