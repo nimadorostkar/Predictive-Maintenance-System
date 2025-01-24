9c1cd6fea9a61c5b474f8391fa1ebe62d31f46e3
nima <54316106+nimadorostkar@users.noreply.github.com>
2025-01-24 18:05:39 +0330

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 
 
+
 # Load the trained model (replace 'model.pkl' with your actual file name)
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
